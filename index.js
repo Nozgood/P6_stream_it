7e58dedd6f61c6ac678e1497a5bc39c53dab6230
nowfeel <nowfeel@bliink.io>
2023-05-31 07:18:34 +0200

    refactor: edit method to get best movies + feat: add loader gif when image is not got

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const FETCH_ERROR = "error during the request"
 const API_ENDPOINT = "http://localhost:8000/api/v1/titles"
-const IMDB_SCORE_MAX = "9.6"
-const IMDB_SCORE_MAX_SECOND = "9.3"
-const BEST_MOVIE_URL = API_ENDPOINT + "?imdb_score_min=" + IMDB_SCORE_MAX
-const SECOND_BEST_MOVIE_URL = API_ENDPOINT + "?imdb_score_min=" + IMDB_SCORE_MAX_SECOND + "&sort_by=" + "-imdb_score"
-const BEST_MOVIE_DIV = document.getElementsByClassName("best__movie")
+const BEST_MOVIE_URL = API_ENDPOINT + "?sort_by=-imdb_score"
 const BEST_MOVIE_TITLE = document.getElementById("best__movie-title")
 const BEST_MOVIE_IMG = document.getElementById("best__movie-img")
 const BEST_MOVIE_DESC = document.getElementById("best__movie-description")
@@ -16,53 +12,52 @@ async function get(URL) {
         console.log(FETCH_ERROR)
         return 
     }
-
     const data = await res.json()
     return data
 }
-async function buildBestMovie(data) {
-    const bestMovie = data.results[3]
-    const singleMovieData = await get(API_ENDPOINT + "/" + bestMovie.id)
 
+async function buildBestMovie(data) {
+    // FETCH FULL INFO OF BEST MOVIE
+    const singleMovieData = await get(API_ENDPOINT + "/" + data.id)
+
+    // INSERT IN THE HTML
     BEST_MOVIE_TITLE.innerHTML = singleMovieData.title
     BEST_MOVIE_IMG.setAttribute("src", singleMovieData.image_url)
     BEST_MOVIE_DESC.innerHTML = singleMovieData.description
 }
 
-async function buildMovie() {
-    let data = await get(BEST_MOVIE_URL)
-    buildBestMovie(data)
-
-    data = await get(SECOND_BEST_MOVIE_URL)
-    console.log(data)
-    let otherBestMovies = []
-    for (let i = 0; i < data.results.length; i++) {
-        if (data.results[i].title != BEST_MOVIE_TITLE.textContent) {
-            otherBestMovies.push(data.results[i])
-        } 
-    }
-    if (data.next != "") {
-        data = await get(data.next)
-        for (let i = 0; i < data.results.length; i++) {
-            if (data.results[i].title != BEST_MOVIE_TITLE.textContent) {
-                otherBestMovies.push(data.results[i])
-                if (otherBestMovies.length == 7) {
-                    i = data.results.length
-                }
-            } 
-        }
-    }
-
-    console.log(otherBestMovies)
-
-    for (let i=0; i < otherBestMovies.length; i++) {
+async function buildSecondBestMovies(movies) {
+    for (let i=0; i < movies.length; i++) {
         let movieTitle = document.getElementById("second__movie-title-" + i)
         let movieImg = document.getElementById("second__movie-img-" + i)
-        movieTitle.innerHTML = otherBestMovies[i].title
-        movieImg.setAttribute("src", otherBestMovies[i].image_url)
+        movieTitle.innerHTML = movies[i].title
+        movieImg.setAttribute("src", movies[i].image_url)
+    }
+}
+
+async function buildMovie() {
+    // FETCH FIRST PART OF BEST MOVIES  
+    let data = await get(BEST_MOVIE_URL)
+    let movies = []
+
+    for (i=0; i < data.results.length; i++) {
+        movies.push(data.results[i])
     }
 
+    // FETCH SECOND PART OF BEST MOVIES 
+    const SECOND_URL = data.next
+    data = await get(SECOND_URL)
+
+    for (i=0; i < 3; i++) {
+        movies.push(data.results[i])
+    }
+
+    buildBestMovie(movies[0])
+    movies.shift()
+    buildSecondBestMovies(movies)
 }
 
 // get and display information about the best movie of the api and the seven others
 buildMovie()
+
+// genres = await get("")
